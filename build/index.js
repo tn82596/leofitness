2ae3bd1467949a3334f79277c2c60ead3d7059e2
rathull <notifyrathul@gmail.com>
2024-02-27 02:35:37 -0800

    test push for eb

--- build/index.js
+++ build/index.js
@@ -7,7 +7,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
-// import mongoose from 'mongoose';
+const mongoose_1 = __importDefault(require("mongoose"));
 const workoutSession_1 = __importDefault(require("./routes/workoutSession"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -16,22 +16,21 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
-    res.send('Server up!');
+    res.send('Server up (updated for testing w/ EB)!');
 });
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-// const uri = process.env.MONGODB_URI || '';
+const uri = process.env.MONGODB_URI || '';
 // connect to mongodb database
-// mongoose
-// 	.connect(uri)
-// 	.then(() => {
-// 		console.log('Successfully connected to MongoDB!');
-// 	})
-// 	.catch((err: mongoose.Error) => {
-// 		mongoose.disconnect();
-// 		console.log('Failed to connect to MongoDB');
-// 		console.log(`Error: ${err}`);
-// 	});
+mongoose_1.default
+    .connect(uri)
+    .then(() => {
+    console.log('Successfully connected to MongoDB!');
+})
+    .catch((err) => {
+    mongoose_1.default.disconnect();
+    console.log('Failed to connect to MongoDB');
+    console.log(`Error: ${err}`);
+});
 app.use('/api/', workoutSession_1.default);
-// node build/index.js
