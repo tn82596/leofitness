ac7629172f99fa10a2618052738279d1efa31495
rathull <notifyrathul@gmail.com>
2024-02-27 02:27:55 -0800

    test commit for eb

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('Server up!');
+	res.send('Server up (updated for testing w/ EB)!');
 });
 
 app.listen(port, () => {
